df037e733df784480955e49c5d822466b1cd98da
KjellSwedin <kjells@uw.edu>
2021-08-17 16:24:16 -0700

    Update to latest released libraries

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
         long_description = f.read()
 
     install_requirements = [
-        'vivarium==0.10.4',
-        'vivarium_public_health==0.10.11',
+        'vivarium==0.10.9',
+        'vivarium_public_health==0.10.12',
 
         'click',
         'gbd_mapping>=3.0.0, <4.0.0',
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
     # use "pip install -e .[dev]" to install required components + extra components
     extras_require = [
-        'vivarium_cluster_tools==1.2.5',
+        'vivarium_cluster_tools==1.2.8',
         'vivarium_inputs[data]==4.0.4',
     ]
 
