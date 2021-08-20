0b8ff9a6de26f358091cd7d77e8c39a181dc29c7
KjellSwedin <kjells@uw.edu>
2021-08-20 15:26:49 -0700

    Use proportion file to modify incidence data for HF-related causes. Modularize special case handling

--- src/vivarium_nih_us_cvd/paths.py
+++ src/vivarium_nih_us_cvd/paths.py
@@ -8,3 +8,5 @@ BASE_DIR = Path(vivarium_nih_us_cvd.__file__).resolve().parent
 ARTIFACT_ROOT = Path(f"/share/costeffectiveness/artifacts/{metadata.PROJECT_NAME}/")
 MODEL_SPEC_DIR = BASE_DIR / 'model_specifications'
 RESULTS_ROOT = Path(f'/share/costeffectiveness/results/{metadata.PROJECT_NAME}/')
+
+HD_PROPDATA_PATH = '/share/scratch/projects/cvd_gbd/cvd_re/simulation_science/hf_props_2021_08_18.csv'
