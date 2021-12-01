9067a913b13ded26472e38f09f47fcbbc2690aee
Matthew Kappel <mkappel@uw.edu>
2021-12-01 13:17:36 -0800

    Correct the scenario column name

--- src/vivarium_nih_us_cvd/constants/results.py
+++ src/vivarium_nih_us_cvd/constants/results.py
@@ -15,7 +15,7 @@ TOTAL_YLLS_COLUMN = 'years_of_life_lost'
 # Columns from parallel runs
 INPUT_DRAW_COLUMN = 'input_draw'
 RANDOM_SEED_COLUMN = 'random_seed'
-OUTPUT_SCENARIO_COLUMN = 'screening_algorithm.scenario'
+OUTPUT_SCENARIO_COLUMN = 'branch_name.scenario'
 
 STANDARD_COLUMNS = {
     'total_population': TOTAL_POPULATION_COLUMN,
